3c07909ab9763530c1d3a6aa54c064f445db6b5b
HammadM48 <hammadmahmood484@gmail.com>
2025-11-11 21:55:25 -0500

    Added theme and dark theme. Minor tweaks to style.

--- lib/widgets/chart/chart.dart
+++ lib/widgets/chart/chart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:expense_tracker/widgets/chart/chart_bar.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -19,28 +18,22 @@ class Chart extends StatelessWidget {
 
   double get maxTotalExpense {
     double maxTotalExpense = 0;
-
     for (final bucket in buckets) {
       if (bucket.totalExpenses > maxTotalExpense) {
         maxTotalExpense = bucket.totalExpenses;
       }
     }
-
     return maxTotalExpense;
   }
 
-  @override
   Widget build(BuildContext context) {
     final isDarkMode =
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Container(
       margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 8,
-      ),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
       width: double.infinity,
-      height: 180,
+      height: 150,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         gradient: LinearGradient(
